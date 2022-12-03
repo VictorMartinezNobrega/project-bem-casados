fab3a4f6544eb375b74841e1c9e116d560e29ac1
Victor Martinez <victormartinobre@hotmail.com>
2022-12-02 21:07:35 -0300

    feat: :construction: adding -structure

---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./styles.scss";
+
+const Footer = () => {
+  return (
+    <div className='div-footer-master'>
+      Footer
+    </div>
+  )
+}
+
+export default Footer;
