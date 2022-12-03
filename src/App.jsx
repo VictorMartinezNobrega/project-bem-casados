eb89db4cb5cdc5afa04f976fe9f82858135287b4
Victor Martinez <victormartinobre@hotmail.com>
2022-12-03 01:55:40 -0300

    style: :construction: add-prototype-navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import './App.scss';
 import Footer from './components/Footer';
 import NavBar from './components/Navbar';
+import Home from './pages/Home';
 
 function App() {
   return (
     <div className="App">
       <NavBar />
-
+      <Home />
       <Footer />
     </div>
   );
--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,8 +1,30 @@
 import React from 'react';
+import "./styles.scss";
 
 const NavBar = () => {
   return (
-    <div>NavBar</div>
+    <div className='div-navbar-master'>
+      <div className='div-navbar'>
+        <div className='div-logo'>
+          <img src="./img/logo-bem-casados.jpg" alt="logo-bem-casados" />
+        </div>
+        <div className='div-navigation'>
+          {/**Local dos botões de navagação */}
+          <button>
+            button1
+          </button>
+          <button>
+            button2
+          </button>
+          <button>
+            button3
+          </button>
+          <button>
+            button4
+          </button>
+        </div>
+      </div>
+    </div>
   )
 }
 
