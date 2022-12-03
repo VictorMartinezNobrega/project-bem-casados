529b00b2eb3bc723c1886d611082757a1f4b828e
Victor Martinez <victormartinobre@hotmail.com>
2022-12-03 03:13:37 -0300

    feat: :construction: add-prototype-footer

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,10 +1,23 @@
 import React from 'react';
+import { FaFacebook, FaInstagram } from 'react-icons/fa';
 import "./styles.scss";
 
 const Footer = () => {
   return (
     <div className='div-footer-master'>
-      Footer
+      <div className='div-footer'>
+        <div className='div-logo'>
+          <img src="./img/logo-bem-casados2.png" alt="segunda-logo" />
+        </div>
+        <div className='div-links'>
+          <div className='div-facebook'>
+            <FaFacebook /> <a href='https://www.facebook.com/vanessamartinez.bemcasados' target="blank">Facebook</a>
+          </div>
+          <div className='div-instagram'>
+            <FaInstagram /> <a href='https://www.instagram.com/vanessam_bemcasadosemacarons/' target="blank">Instagram</a>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
