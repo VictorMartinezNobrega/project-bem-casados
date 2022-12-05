173864f8764c83d0293b968fe467ea4b0774ed42
Victor Martinez <victormartinobre@hotmail.com>
2022-12-04 22:45:56 -0300

    feat: :construction: add-banner

---
+++ src/components/Banner/index.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import "./styles.scss";
+
+const index = () => {
+  return (
+    <div className='div-banner-master'>
+        <div className='div-banner'>
+            <div className='div-img-banner'>
+                <img id='img3' src="./img/imgBanner3.jpg" alt="Imagem de bem casados" />
+            </div>
+            <div className='div-text'>
+                <h1>
+                    Texto de exemplo
+                </h1>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default index;
