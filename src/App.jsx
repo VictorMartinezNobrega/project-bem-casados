6d07e2002ca5ac4cdfa70cf1d7d4a360c0755728
Victor Martinez <victormartinobre@hotmail.com>
2022-12-07 10:51:34 -0300

    feat: :lipstick: add-chakra-in-project

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,16 @@ import './App.scss';
 import Footer from './components/Footer';
 import NavBar from './components/Navbar';
 import Home from './pages/Home';
+import { ChakraProvider } from '@chakra-ui/react';
 
 function App() {
   return (
     <div className="App">
-      <NavBar />
-      <Home />
-      <Footer />
+      <ChakraProvider>
+        <NavBar />
+        <Home />
+        <Footer />
+      </ChakraProvider>
     </div>
   );
 }
