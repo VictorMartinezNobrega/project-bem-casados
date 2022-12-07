f810c9aaf784449bcb0604900d4dc08c1d57498a
Victor Martinez <victormartinobre@hotmail.com>
2022-12-07 10:53:57 -0300

    style: :art: code-formatting

--- src/components/Banner/index.jsx
+++ src/components/Banner/index.jsx
@@ -2,20 +2,23 @@ import React from 'react';
 import "./styles.scss";
 
 const index = () => {
-  return (
-    <div className='div-banner-master'>
-        <div className='div-banner'>
-            <div className='div-img-banner'>
-                <img id='img3' src="./img/imgBanner3.jpg" alt="Imagem de bem casados" />
-            </div>
-            <div className='div-text'>
-                <h1>
-                    Texto de exemplo
-                </h1>
+    return (
+        <div className='div-banner-master'>
+            <div className='div-banner'>
+                <div className='div-img-banner'>
+                    <img id='img3' src="./img/imgBanner3.jpg" alt="Imagem de bem casados" />
+                </div>
+                <div className='div-text'>
+                    <h1>
+                        História do Bem Casado
+                    </h1>
+                    <p>
+                        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Culpa ab dolor suscipit repellendus qui, in porro unde inventore, necessitatibus voluptatem voluptates dolore, neque ad beatae! Necessitatibus qui facere modi! Dolorem.
+                    </p>
+                </div>
             </div>
         </div>
-    </div>
-  )
+    )
 }
 
 export default index;
