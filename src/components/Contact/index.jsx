541af13f5f7d5da9dbf1a8d4601ba47facb73c11
Victor Martinez <victormartinobre@hotmail.com>
2022-12-07 23:34:27 -0300

    feat: :sparkles: update-in-contact-and-add-chakra

--- src/components/Contact/index.jsx
+++ src/components/Contact/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { FaFacebook, FaInstagram, FaWhatsapp } from 'react-icons/fa';
-import { Card, CardBody } from '@chakra-ui/react';
-import { Stack } from '@chakra-ui/react';
+import { Card, CardHeader, CardBody, Stack, Link, Box, Heading } from '@chakra-ui/react';
+import { ExternalLinkIcon } from '@chakra-ui/icons';
 
 import './styles.scss';
 
@@ -9,27 +9,37 @@ const index = () => {
   return (
     <div className='div-contact-master'>
       <div className='div-contact'>
-        <div className='div-space'>
-          <div className='div-whats'>
-            <h1>Entre em contato!</h1>
-            <div className='div-icon-whats'>
-              <FaWhatsapp /> <a href="http://api.whatsapp.com/send?1=pt_BR&phone=5513981483289" target="blank">Whatsapp</a>
-            </div>
-          </div>
-          <Card>
-            <CardBody bgColor={"green"}>
-              <h1>Entre em contato!</h1>
-              <Stack display={'flex'} direction='row'>
-                <FaWhatsapp /><a href="http://api.whatsapp.com/send?1=pt_BR&phone=5513981483289" target="blank">Whatsapp</a>
+        <Box border={2} borderColor={'red'} w={'100%'}>
+          <Card
+            border={'2px'}
+            borderColor={'#a36483'}
+            bgColor={"#ffdcfe"}
+            align={'center'}
+          >
+            <CardHeader>
+              <Heading size='4xl'>Entre em contato!</Heading>
+            </CardHeader>
+            <CardBody>
+              <Stack display={'flex'} direction='row' alignItems={'center'}>
+                <FaWhatsapp size={25}  />
+                <Link className='link' href='http://api.whatsapp.com/send?1=pt_BR&phone=5513981483289' isExternal>
+                  Whatsapp
+                  <ExternalLinkIcon h={3} />
+                </Link>
+                <FaFacebook size={25} />
+                <Link className='link' href='https://www.facebook.com/vanessamartinez.bemcasados' isExternal>
+                  Facebook
+                  <ExternalLinkIcon h={3} />
+                </Link>
+                <FaInstagram size={25} />
+                <Link className='link' href='https://www.instagram.com/vanessam_bemcasadosemacarons/' isExternal>
+                  Instagram
+                  <ExternalLinkIcon h={3}/>
+                </Link>
               </Stack>
             </CardBody>
           </Card>
-          <div className='div-social'>
-            <h1>Conheça o meu trabalho!</h1>
-            <FaFacebook /> <a href='https://www.facebook.com/vanessamartinez.bemcasados' target="blank">Facebook</a>
-            <FaInstagram /> <a href='https://www.instagram.com/vanessam_bemcasadosemacarons/' target="blank">Instagram</a>
-          </div>
-        </div>
+        </Box>
       </div>
     </div>
   )
