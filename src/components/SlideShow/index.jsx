3a92943e7b7e8b102d440151d0c690af958201c9
Victor Martinez <victormartinobre@hotmail.com>
2023-01-16 18:05:07 -0300

    feat: :sparkles: add-carousel-and-images

---
+++ src/components/SlideShow/index.jsx
@@ -0,0 +1,57 @@
+import React from 'react';
+import "./styles.scss";
+
+function SlideShow() {
+  return (
+    <div>
+      <div class="slider">
+        <div class="slide-track">
+          <div class="slide">
+            <img src="./img/carrosel1.jpg" alt="" />
+          </div>
+          <div class="slide">
+            <img src="./img/carrosel2.jpg"   alt="" />
+          </div>
+          <div class="slide">
+            <img src="./img/carrosel4.jpg"   alt="" />
+          </div>
+          <div class="slide">
+            <img src="./img/carrosel5.jpg"   alt="" />
+          </div>
+          <div class="slide">
+            <img src="./img/carrosel6.jpg"   alt="" />
+          </div>
+          <div class="slide">
+            <img src="./img/carrosel7.jpg"   alt="" />
+          </div>
+          <div class="slide">
+            <img src="./img/carrosel8.jpg"   alt="" />
+          </div>
+          <div class="slide">
+            <img src="./img/carrosel1.jpg" alt="" />
+          </div>
+          <div class="slide">
+            <img src="./img/carrosel2.jpg"   alt="" />
+          </div>
+          <div class="slide">
+            <img src="./img/carrosel4.jpg"   alt="" />
+          </div>
+          <div class="slide">
+            <img src="./img/carrosel5.jpg"   alt="" />
+          </div>
+          <div class="slide">
+            <img src="./img/carrosel6.jpg"   alt="" />
+          </div>
+          <div class="slide">
+            <img src="./img/carrosel7.jpg"   alt="" />
+          </div>
+          <div class="slide">
+            <img src="./img/carrosel8.jpg"   alt="" />
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default SlideShow;
