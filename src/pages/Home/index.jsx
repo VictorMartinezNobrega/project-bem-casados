a79f45302e10f4f0308bab469be9daaaa2084f2e
Victor Martinez <victormartinobre@hotmail.com>
2023-01-16 18:03:08 -0300

    feat: :sparkles: add-button-go-to-top

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,15 +1,34 @@
 import React from 'react';
+import SlideShow from '../../components/SlideShow';
 import Banner from '../../components/Banner';
 import Budget from '../../components/Budget';
 import Contact from '../../components/Contact';
+import { ChevronUpIcon } from '@chakra-ui/icons';
+import { Button } from '@chakra-ui/react';
 import "./styles.scss";
 
 const index = () => {
+  
+  const goTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  };
+
   return (
     <div className='div-home-master'>
+      <SlideShow />
       <Banner />
       <Budget />
       <Contact />
+      
+      <Button onClick={goTop}
+      className='button-to-top' variant='outline' colorScheme='pink' 
+      position={'fixed'} top={'80%'} left={'96%'} width={'40px'} height={'40px'}
+      >
+        <ChevronUpIcon h={5}/>
+      </Button>
     </div>
   )
 }
