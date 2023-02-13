aa59d0dca1a4cbfb66a3e3b6ddad34bf5c76d434
Victor Martinez <victormartinobre@hotmail.com>
2023-02-13 17:44:23 -0300

    feat: :sparkles: add-city-and-phone

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,3 +1,4 @@
+import { Box, Text } from '@chakra-ui/react';
 import React from 'react';
 import { FaFacebook, FaInstagram } from 'react-icons/fa';
 import "./styles.scss";
@@ -9,6 +10,9 @@ const Footer = () => {
         <div className='div-logo'>
           <img src="./img/logo-bc2.png" alt="segunda-logo" />
         </div>
+        <Box m={'auto'} ml={'10px'}>
+          <Text fontSize={'1vw'} color={'white'}>Praia Grande - SP / &#40;13&#41; 98148-3289</Text>
+        </Box>
         <div className='div-links'>
           <div className='div-facebook'>
             <FaFacebook /> <a href='https://www.facebook.com/vanessamartinez.bemcasados' target="blank">Facebook</a>
