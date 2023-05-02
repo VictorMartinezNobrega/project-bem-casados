0e267a791ef0607c63dad668a00f9545efcad752
Victor Martinez <victormartinobre@hotmail.com>
2023-05-02 16:36:06 -0300

    feat: :sparkles: add-all-rights-reserved

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -6,12 +6,19 @@ const Footer = () => {
   return (
     <div className='div-footer-master'>
       <div className='div-footer'>
-        <div className='div-logo'>
-          <img src="./img/logo-bc2.png" alt="segunda-logo" />
+        <div className='div-infos'>
+          <div className='div-logo'>
+            <img src="./img/logo-bc2.png" alt="segunda-logo" />
+          </div>
+          <Box m={'auto'} ml={'10px'}>
+            <Text fontSize={'15px'} color={'white'}>Praia Grande - SP / &#40;13&#41; 98148-3289</Text>
+          </Box>
         </div>
-        <Box m={'auto'} ml={'10px'}>
-          <Text fontSize={'15px'} color={'white'}>Praia Grande - SP / &#40;13&#41; 98148-3289</Text>
-        </Box>
+        <div className='div-rights'>
+          <Box ml={'auto'} mr={'auto'}>
+            <Text fontSize={'10px'}>Todos os direitos reservados</Text>
+          </Box>
+        </div>
       </div>
     </div>
   )
