6d7dd41c4fcf03e6dfda9ec67b9e96bf1a5b190a
Victor Martinez <victormartinobre@hotmail.com>
2023-05-02 17:29:33 -0300

    feat: :sparkles: att-button-go-top

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import { useState, useEffect } from 'react';
 import SlideShow from '../../components/SlideShow';
 import Banner from '../../components/Banner';
 import Budget from '../../components/Budget';
@@ -7,12 +7,30 @@ import { ChevronUpIcon } from '@chakra-ui/icons';
 import { Button } from '@chakra-ui/react';
 import "./styles.scss";
 
-const index = () => {
-  
+const Home = () => {
+
+  const [isVisible, setIsVisible] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.pageYOffset < 200) {
+        setIsVisible(true);
+      } else {
+        setIsVisible(false);
+      }
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
   const goTop = () => {
     window.scrollTo({
       top: 0,
-      behavior: "smooth",
+      behavior: 'smooth'
     });
   };
 
@@ -22,15 +40,16 @@ const index = () => {
       <Banner />
       <Budget />
       <Contact />
-      
+
       <Button onClick={goTop}
-      className='button-to-top' variant='outline' colorScheme='pink' 
-      position={'fixed'} top={'80%'} right={'1%'} width={'40px'} height={'40px'}
+        className={`button-to-top ${isVisible ? 'show' : ''}`}
+        variant='outline' colorScheme='pink'
+        position={'fixed'} top={'80%'} right={'1%'} width={'40px'} height={'40px'}
       >
-        <ChevronUpIcon h={5}/>
+        <ChevronUpIcon h={5} />
       </Button>
     </div>
   )
 }
 
-export default index;
+export default Home;
