0ae3e4a3af6631b321886b7812cfedccf9e54006
Victor Martinez <victormartinobre@hotmail.com>
2023-05-02 16:28:56 -0300

    fix: :bug: tranform-class-in-classname

--- src/components/SlideShow/index.jsx
+++ src/components/SlideShow/index.jsx
@@ -4,73 +4,73 @@ import "./styles.scss";
 function SlideShow() {
   return (
     <div>
-      <div class="slider">
-        <div class="slide-track">
-          <div class="slide">
+      <div className="slider">
+        <div className="slide-track">
+          <div className="slide">
             <img src="./img/carrosel1.jpg" alt="" />
           </div>
-          <div class="slide">
-            <img src="./img/carrosel2.jpg"   alt="" />
+          <div className="slide">
+            <img src="./img/carrosel2.jpg" alt="" />
           </div>
-          <div class="slide">
-            <img src="./img/carrosel3.jpg"   alt="" />
+          <div className="slide">
+            <img src="./img/carrosel3.jpg" alt="" />
           </div>
-          <div class="slide">
-            <img src="./img/carrosel4.jpg"   alt="" />
+          <div className="slide">
+            <img src="./img/carrosel4.jpg" alt="" />
           </div>
-          <div class="slide">
-            <img src="./img/carrosel5.jpg"   alt="" />
+          <div className="slide">
+            <img src="./img/carrosel5.jpg" alt="" />
           </div>
-          <div class="slide">
-            <img src="./img/carrosel6.jpg"   alt="" />
+          <div className="slide">
+            <img src="./img/carrosel6.jpg" alt="" />
           </div>
-          <div class="slide">
-            <img src="./img/carrosel7.jpg"   alt="" />
+          <div className="slide">
+            <img src="./img/carrosel7.jpg" alt="" />
           </div>
-          <div class="slide">
-            <img src="./img/carrosel8.jpg"   alt="" />
+          <div className="slide">
+            <img src="./img/carrosel8.jpg" alt="" />
           </div>
-          <div class="slide">
-            <img src="./img/carrosel9.jpg"   alt="" />
+          <div className="slide">
+            <img src="./img/carrosel9.jpg" alt="" />
           </div>
-          <div class="slide">
-            <img src="./img/carrosel10.jpg"   alt="" />
+          <div className="slide">
+            <img src="./img/carrosel10.jpg" alt="" />
           </div>
-          <div class="slide">
-            <img src="./img/carrosel11.jpg"   alt="" />
+          <div className="slide">
+            <img src="./img/carrosel11.jpg" alt="" />
           </div>
-          <div class="slide">
+          <div className="slide">
             <img src="./img/carrosel1.jpg" alt="" />
           </div>
-          <div class="slide">
-            <img src="./img/carrosel2.jpg"   alt="" />
+          <div className="slide">
+            <img src="./img/carrosel2.jpg" alt="" />
           </div>
-          <div class="slide">
-            <img src="./img/carrosel3.jpg"   alt="" />
+          <div className="slide">
+            <img src="./img/carrosel3.jpg" alt="" />
           </div>
-          <div class="slide">
-            <img src="./img/carrosel4.jpg"   alt="" />
+          <div className="slide">
+            <img src="./img/carrosel4.jpg" alt="" />
           </div>
-          <div class="slide">
-            <img src="./img/carrosel5.jpg"   alt="" />
+          <div className="slide">
+            <img src="./img/carrosel5.jpg" alt="" />
           </div>
-          <div class="slide">
-            <img src="./img/carrosel6.jpg"   alt="" />
+          <div className="slide">
+            <img src="./img/carrosel6.jpg" alt="" />
           </div>
-          <div class="slide">
-            <img src="./img/carrosel7.jpg"   alt="" />
+          <div className="slide">
+            <img src="./img/carrosel7.jpg" alt="" />
           </div>
-          <div class="slide">
-            <img src="./img/carrosel8.jpg"   alt="" />
+          <div className="slide">
+            <img src="./img/carrosel8.jpg" alt="" />
           </div>
-          <div class="slide">
-            <img src="./img/carrosel9.jpg"   alt="" />
+          <div className="slide">
+            <img src="./img/carrosel9.jpg" alt="" />
           </div>
-          <div class="slide">
-            <img src="./img/carrosel10.jpg"   alt="" />
+          <div className="slide">
+            <img src="./img/carrosel10.jpg" alt="" />
           </div>
-          <div class="slide">
-            <img src="./img/carrosel11.jpg"   alt="" />
+          <div className="slide">
+            <img src="./img/carrosel11.jpg" alt="" />
           </div>
         </div>
       </div>
